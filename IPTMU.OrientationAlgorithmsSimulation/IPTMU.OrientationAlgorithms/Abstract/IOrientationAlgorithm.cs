9347fb51e37ad0414778cfc394906c5149666657
biryukovvg <biryukovvg@gmail.com>
2021-06-18 00:01:27 +0400

    Заготовка для реализации алгоритмов ориентации

---
+++ IPTMU.OrientationAlgorithmsSimulation/IPTMU.OrientationAlgorithms/Abstract/IOrientationAlgorithm.cs
@@ -0,0 +1,14 @@
+﻿using IPTMU.MathKernel.OrientationParameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPTMU.OrientationAlgorithms.Abstract
+{
+    public interface IOrientationAlgorithm
+    {
+        Quaternion Calculate(Quaternion lambdaPrevious, Quaternion omegaPrevious, Quaternion omegaCurrent);
+    }
+}
