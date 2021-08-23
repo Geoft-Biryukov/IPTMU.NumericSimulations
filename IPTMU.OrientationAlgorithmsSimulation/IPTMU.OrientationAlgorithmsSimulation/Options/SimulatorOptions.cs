d35b33582125a94f2b2f33d96ca91c66321a9398
biryukovvg <biryukovvg@gmail.com>
2021-08-24 00:20:24 +0400

    Начал делать управление моделированием

---
+++ IPTMU.OrientationAlgorithmsSimulation/IPTMU.OrientationAlgorithmsSimulation/Options/SimulatorOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPTMU.OrientationAlgorithmsSimulation.Options
+{
+    internal class SimulatorOptions
+    {
+        public double IntegrationStep { get; set; }
+        public double MotionTime { get; set; }
+        public OrientationAlgorithms Algorithm { get; set; }
+        public MotionSimulators MotionSimulator { get; set; }
+    }
+}
---
+++ IPTMU.OrientationAlgorithmsSimulation/IPTMU.OrientationAlgorithmsSimulation/OrientationAlgorithmsSimulator.cs
@@ -0,0 +1,49 @@
+﻿using IPTMU.AngularMotionSimulation.Abstract;
+using IPTMU.AngularMotionSimulation.Concrete;
+using IPTMU.OrientationAlgorithms.Abstract;
+using IPTMU.OrientationAlgorithmsSimulation.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPTMU.OrientationAlgorithmsSimulation
+{
+    internal class OrientationAlgorithmsSimulator
+    {
+        
+        public void StartSimulation(SimulatorOptions options)
+        {
+            if (options is null) 
+                throw new ArgumentNullException(nameof(options));
+
+            var algorithm = CreateAlgorithm(options.Algorithm, options.IntegrationStep);
+           // IAngularMotionSimulator motionSimulator = CreateMotionSimulator(options.MotionSimulator);
+        }        
+
+        private IOrientationAlgorithm CreateAlgorithm(Options.OrientationAlgorithms algorithm, double integrationStep)
+        {
+            switch (algorithm)
+            {
+                case Options.OrientationAlgorithms.Molodenkov:
+                    return new OrientationAlgorithms.Concrete.Molodenkov.OrientationAlgorithm(integrationStep);
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        //private IAngularMotionSimulator CreateMotionSimulator(MotionSimulators motionSimulator)
+        //{
+        //    switch (motionSimulator)
+        //    {
+        //        case MotionSimulators.ClassicalConingMotion:
+        //            return new ClassicalConingMotionSimulator()
+        //        case MotionSimulators.GeneralizedConicalPrecession:
+        //            break;
+        //        default:
+        //            throw new NotSupportedException(nameof(MotionSimulators));
+        //    }
+        //}
+    }
+}
