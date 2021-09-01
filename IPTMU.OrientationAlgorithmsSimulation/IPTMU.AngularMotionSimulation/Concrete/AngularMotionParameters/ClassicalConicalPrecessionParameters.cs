a1a227a6692512a241e2b99ceb49b1d27c83a7db
biryukovvg <biryukovvg@gmail.com>
2021-09-01 23:46:00 +0400

    Начал делать прецессию по Бранецу и Шмыглевскому

---
+++ IPTMU.OrientationAlgorithmsSimulation/IPTMU.AngularMotionSimulation/Concrete/AngularMotionParameters/ClassicalConicalPrecessionParameters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPTMU.AngularMotionSimulation.Logic.Concrete.AngularMotionParameters
+{
+    /// <summary>
+    /// Классическая прецессия
+    /// В.Н. Бранец, И.П. Шмыглевский
+    /// omega = a * i1 * cos(b*t) + a * i2 * sin(b*t) + c
+    /// </summary>
+    public class ClassicalConicalPrecessionParameters
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+    }
+}
---
+++ IPTMU.OrientationAlgorithmsSimulation/IPTMU.AngularMotionSimulation/Concrete/ClassicalConicalPrecessionSimulator.cs
@@ -0,0 +1,33 @@
+﻿using IPTMU.AngularMotionSimulation.Abstract;
+using IPTMU.AngularMotionSimulation.Logic.Concrete.AngularMotionParameters;
+using IPTMU.Auxiliaries.DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPTMU.AngularMotionSimulation.Logic.Concrete
+{
+    /// <summary>
+    /// Классическая прецессия
+    /// В.Н. Бранец, И.П. Шмыглевский
+    /// omega = a * i1 * cos(b * t) + a * i2 * sin(b * t) + c * i3
+    /// lambda = exp(B * t / 2) * exp((omega0 - B) * t / 2), 
+    /// где B = b * i3
+    /// </summary>
+    public class ClassicalConicalPrecessionSimulator : IAngularMotionSimulator
+    {
+        private readonly ClassicalConicalPrecessionParameters parameters;
+
+        public ClassicalConicalPrecessionSimulator(ClassicalConicalPrecessionParameters parameters)
+        {
+            this.parameters = parameters ?? throw new ArgumentNullException(nameof(parameters));
+        }
+
+        public AngularState GetAngularMotion(double t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
