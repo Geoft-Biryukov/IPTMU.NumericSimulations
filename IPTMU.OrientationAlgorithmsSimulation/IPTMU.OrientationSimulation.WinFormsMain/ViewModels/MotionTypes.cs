74a90e58f267eb071f0fa1a46c2c8991713acbc9
Vyacheslav Biryukov <geoft.biryukov@gmail.com>
2021-10-25 21:02:23 +0400

    Удалил ненужные using

--- IPTMU.OrientationAlgorithmsSimulation/IPTMU.OrientationSimulation.WinFormsMain/ViewModels/MotionTypes.cs
+++ IPTMU.OrientationAlgorithmsSimulation/IPTMU.OrientationSimulation.WinFormsMain/ViewModels/MotionTypes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IPTMU.OrientationSimulation.WinFormsMain.ViewModels
+﻿namespace IPTMU.OrientationSimulation.WinFormsMain.ViewModels
 {
     public enum MotionTypes
     {
